652df86f815ff2424deb6c687e3372ab195effe8
Tomas Votruba <tomas.vot@gmail.com>
2022-05-15 23:07:12 +0200

    add abstract macro test

---
+++ tests/AbstractEmbeddedMacroTest.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Milo\EmbeddedSvg\Tests;
+
+use Iterator;
+use Latte\Engine;
+use Latte\Loaders\StringLoader;
+use Nette\Bridges\ApplicationLatte\LatteFactory;
+use Nette\Utils\Strings;
+use PHPUnit\Framework\TestCase;
+use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
+use Symplify\EasyTesting\DataProvider\StaticFixtureUpdater;
+use Symplify\EasyTesting\StaticFixtureSplitter;
+use Symplify\SmartFileSystem\SmartFileInfo;
+
+abstract class AbstractEmbeddedMacroTest extends TestCase
+{
+    private Engine $latteEngine;
+
+    abstract public function provideConfig(): string;
+
+    abstract public function provideFixtureDirectory(): string;
+
+    protected function setUp(): void
+    {
+        $containerFactory = new ContainerFactory();
+        $container = $containerFactory->createFromConfig($this->provideConfig());
+
+        // __DIR__ . '/config/test_config.neon'
+
+        /** @var LatteFactory $latteFactory */
+        $latteFactory = $container->getByType(LatteFactory::class);
+
+        $this->latteEngine = $latteFactory->create();
+        $this->latteEngine->setLoader(new StringLoader());
+    }
+
+    /**
+     * @dataProvider provideData()
+     */
+    public function test(SmartFileInfo $fixtureFileInfo): void
+    {
+        $inputAndExpected = StaticFixtureSplitter::splitFileInfoToInputAndExpected($fixtureFileInfo);
+
+        $inputLatteContent = $inputAndExpected->getInput();
+        $expectedCompiledPhpContent = $inputAndExpected->getExpected();
+
+        $compiledPhpCode = $this->compileLatteToPhpContent($inputLatteContent);
+
+        // update tests on change
+        StaticFixtureUpdater::updateFixtureContent(
+            $inputLatteContent,
+            $compiledPhpCode,
+            $fixtureFileInfo
+        );
+
+        $this->assertEquals($expectedCompiledPhpContent, $compiledPhpCode);
+    }
+
+    public function provideData(): Iterator
+    {
+        // @see https://github.com/symplify/easy-testing
+        // @see https://tomasvotruba.com/blog/2020/07/20/how-to-update-hundreds-of-test-fixtures-with-single-phpunit-run/
+        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture', '*.latte');
+    }
+
+    private function compileLatteToPhpContent(string $latteContent): string
+    {
+        //  string $inputLatteContent, string $expectedCompiledPhpContent
+        $compiledPhpCode = $this->latteEngine->compile($latteContent);
+
+        // use spaces to unite all files via .editorconfig
+        return Strings::replace($compiledPhpCode, "#\t#", '    ');
+    }
+}
--- tests/EmbeddedMacroTest.php
+++ tests/EmbeddedMacroTest.php
@@ -4,68 +4,15 @@ declare(strict_types=1);
 
 namespace Milo\EmbeddedSvg\Tests;
 
-use Iterator;
-use Latte\Engine;
-use Latte\Loaders\StringLoader;
-use Nette\Bridges\ApplicationLatte\LatteFactory;
-use Nette\Utils\Strings;
-use PHPUnit\Framework\TestCase;
-use Symplify\EasyTesting\DataProvider\StaticFixtureFinder;
-use Symplify\EasyTesting\DataProvider\StaticFixtureUpdater;
-use Symplify\EasyTesting\StaticFixtureSplitter;
-use Symplify\SmartFileSystem\SmartFileInfo;
-
-final class EmbeddedMacroTest extends TestCase
+final class EmbeddedMacroTest extends AbstractEmbeddedMacroTest
 {
-    private Engine $latteEngine;
-
-    protected function setUp(): void
+    public function provideConfig(): string
     {
-        $containerFactory = new ContainerFactory();
-        $container = $containerFactory->createFromConfig(__DIR__ . '/config/test_config.neon');
-
-        /** @var LatteFactory $latteFactory */
-        $latteFactory = $container->getByType(LatteFactory::class);
-
-        $this->latteEngine = $latteFactory->create();
-        $this->latteEngine->setLoader(new StringLoader());
+        return __DIR__ . '/config/test_config.neon';
     }
 
-    /**
-     * @dataProvider provideData()
-     */
-    public function test(SmartFileInfo $fixtureFileInfo): void
+    public function provideFixtureDirectory(): string
     {
-        $inputAndExpected = StaticFixtureSplitter::splitFileInfoToInputAndExpected($fixtureFileInfo);
-
-        $inputLatteContent = $inputAndExpected->getInput();
-        $expectedCompiledPhpContent = $inputAndExpected->getExpected();
-
-        $compiledPhpCode = $this->compileLatteToPhpContent($inputLatteContent);
-
-        // update tests on change
-        StaticFixtureUpdater::updateFixtureContent(
-            $inputLatteContent,
-            $compiledPhpCode,
-            $fixtureFileInfo
-        );
-
-        $this->assertEquals($expectedCompiledPhpContent, $compiledPhpCode);
-    }
-
-    public function provideData(): Iterator
-    {
-        // @see https://github.com/symplify/easy-testing
-        // @see https://tomasvotruba.com/blog/2020/07/20/how-to-update-hundreds-of-test-fixtures-with-single-phpunit-run/
-        return StaticFixtureFinder::yieldDirectory(__DIR__ . '/Fixture', '*.latte');
-    }
-
-    private function compileLatteToPhpContent(string $latteContent): string
-    {
-        //  string $inputLatteContent, string $expectedCompiledPhpContent
-        $compiledPhpCode = $this->latteEngine->compile($latteContent);
-
-        // use spaces to unite all files via .editorconfig
-        return Strings::replace($compiledPhpCode, "#\t#", '    ');
+        return __DIR__ . '/Fixture';
     }
 }
